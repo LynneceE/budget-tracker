c97e3dc0739d756bf789f64c40d198ca95655453
bhari eutsey <bharilynn@outlook.com>
2022-03-20 23:14:34 -0500

    still fixing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-mongoose.connect = (process.env.MONGODB_URI || "mongodb://localhost:27017/budget", { useMongoClient: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/budget"; 
 
 const app = express();
 
